13cc14aedfffc2740acd915415cbdfab0661ab8c
Márcio Santos do Carmo <marcio.carmo@acad.pucrs.br>
2019-05-27 20:48:35 -0300

    Final commit (i hope)

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) {
         TXTFileChooser txtChooser = new TXTFileChooser();
         String filePath = txtChooser.getPath();
-        String fileName = filePath.substring(8);
 
         Graph graph = new Graph(new In(filePath));
         System.out.println("It`ll be possible to conquer "+ graph.conquered() + " castles!");
